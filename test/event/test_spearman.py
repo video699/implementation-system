e79d25597fc428c41fd283fffdbaf6f7eff2f919
Vít Novotný <witiko@mail.muni.cz>
2018-09-22 19:18:22 +0200

    Implement Benjamini-Hochberg (1995) FDR controlling procedure

---
+++ test/event/test_spearman.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from numpy.testing import assert_array_almost_equal
+from video699.event.spearman import benjamini_hochberg
+
+
+class TestBenjaminiHochberg(unittest.TestCase):
+    """Tests the ability of the benjamini_hochberg method to compute adjusted p-values.
+
+    """
+
+    def test_empty(self):
+        p_values = ()
+        q_values = tuple(benjamini_hochberg(p_values))
+        self.assertEqual((), q_values)
+
+    def test_nonempty(self):
+        p_values = (0.005, 0.009, 0.05, 0.1, 0.2, 0.3)
+        q_values = tuple(benjamini_hochberg(p_values))
+        assert_array_almost_equal((0.027, 0.027, 0.100, 0.150, 0.240, 0.300), q_values)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- video699/event/spearman.py
+++ video699/event/spearman.py
@@ -2,145 +2,44 @@
 
 r"""This module implements a screen event detector that matches document page image data with
 projection screen image data using the Spearman's rank correlation coefficient :math:`\rho` with a
-sliding window of video frames. Related classes are also implemented.
+sliding window of video frames. Related classes and methods are also implemented.
 
 """
 
-from collections import deque
-from itertools import chain
-
-from ..configuration import get_configuration
-from ..interface import EventDetectorABC
-from ..quadrangle.rtree import RTreeConvexQuadrangleIndex
-# from .screen import ScreenAppearedEvent, ScreenChangedContentEvent, ScreenMovedEvent, \
-#     ScreenDisappearedEvent
+import numpy as np
 
 
-CONFIGURATION = get_configuration()['SlidingSpearmanEventDetector']
-WINDOW_SIZE = int(CONFIGURATION['window_size'])
-# ALPHA = float(CONFIGURATION['alpha'])
+def benjamini_hochberg(p_values):
+    """Adjusts p-values from independent hypothesis tests to q-values.
 
+    The q-values are determined using the false discovery rate (FDR) controlling procedure of
+    Benjamini and Hochberg [BenjaminiHochberg1995]_.
 
-class _TrackedScreen(object):
-    """The movement of a uniquely identified lit projection screen in a sliding time window.
+    .. [BenjaminiHochberg1995] Benjamini, Yoav; Hochberg, Yosef (1995). "Controlling the false
+       discovery rate: a practical and powerful approach to multiple testing". Journal of the
+       Royal Statistical Society, Series B. 57 (1): 289–300. MR 1325392.
 
-    Parameters
-    ----------
-    screen_id : str
-        A screen identifier unique among the screen identifiers produced by a screen tracker.
-    previous_screen : ScreenABC
-        The screen detected in the previous video frame.
+    Notes
+    -----
+    This method was adapted from `code posted to Stack Overflow by Eric Talevich`_.
 
-    Attributes
-    ----------
-    screen_id : str
-        A screen identifier unique among the screen identifiers produced by a screen tracker.
-    screens : deque of ScreenABC
-        The movement of a lit projection screen in a sliding time window.
-    previous_screen : ScreenABC
-        The screen detected in the previous video frame.
-    """
-
-    def __init__(self, screen_id, previous_screen):
-        self.screen_id = screen_id
-        self.screens = deque((previous_screen,), maxlen=WINDOW_SIZE)
-
-    @property
-    def previous_screen(self):
-        return self.screens[0]
-
-
-class _ScreenTracker(object):
-    """A tracker that records the movement of lit projection screens in a sliding time window.
-
-    Attributes
-    ----------
-    tracked_screens : a set-like object of _TrackedScreen
-        The tracked lit projection screens.
-    """
-
-    def __init__(self):
-        self._screens = {}
-        self._quadrangle_index = RTreeConvexQuadrangleIndex()
-
-    def screens(self):
-        return self._screens.values()
-
-    def update(self, screens):
-        """Records lit projection screens that were detected in a video frame.
-
-        The detected lit projection screens are compared with the screens in the previous video
-        frame. The detected screens that cross no previous screens are added to the tracker. The
-        detected screens that cross at least one previous screen are linked to the previous screen
-        with the largest intersection area. The previous screens that cross none of the detected
-        screens are removed from the tracker.
-
-        Parameters
-        ----------
-        screens : iterable of ScreenABC
-            The lit projection screens that were detected in a video frame.
-
-        Returns
-        -------
-        appeared : set of _TrackedScreen
-            The detected screens that cross no previous screens.
-        existed : set of _TrackedScreen
-            The detected screens that cross at least one previous screen.
-        disappeared : set of _TrackedScreen
-            The previous screens that cross none of the detected screens.
-
-        """
-        # TODO
-        pass
-
-
-class SlidingSpearmanEventDetector(EventDetectorABC):
-    r"""A screen event detector using Spearman's rank correlation coefficient with a sliding window.
-
-    In each frame of a video, screens are detected using a supplied screen detector. The detected
-    screens are compared with the screens in the previous video frame. The detected screens that
-    cross no previous screens and *match* a document page (more on that later) are reported in a
-    `ScreenAppearedEvent` event.  The detected screens that cross at least one previous screen are
-    considered to correspond to the previous screen with the largest intersection area; a change of
-    coordinates is reported in a `ScreenMovedEvent` event and a change of a matching page is
-    reported in a `ScreenChangedContentEvent` event. The previous screens that cross none of the
-    detected screens or that no longer match a page are reported in a `ScreenDisappearedEvent`
-    event.
-
-    Individual color components of image data pixels in a screen are taken to be a simple random
-    sample :math:`X` (although the independence assumption is clearly violated). Color components
-    with a corresponding alpha value of zero are disregarded. A temporal sliding window is used to
-    increase the sample size. A document page produces a random sample :math:`Y` of the same size.
-    Spearman's correlation coefficient :math:`\rho` between :math:`X` and :math:`Y` is computed. A
-    significance test is performed to see if :math:`\rho` is sufficiently extreme to refuse the null
-    hypothesis :math:`h_0: \rho = 0`. The page with the most extreme significant value of
-    :math:`\rho` is said to *match* the screen. If no page has a significant value of :math:`\rho`,
-    then no page matches the screen.
+    .. _code posted to Stack Overflow by Eric Talevich: https://stackoverflow.com/a/33532498/657401
 
     Parameters
     ----------
-    video : VideoABC
-        The video in which the events are detected.
-    screen_detector : ScreenDetectorABC
-        A screen detector that will be used to detect lit projection screens in video frames.
-    documents : set of DocumentABC
-        Documents whose pages are matched against detected lit projection screens.
+    p_values : iterable of scalar
+        p-values from independent hypothesis tests.
 
-    Attributes
-    ----------
-    video : VideoABC
-        The video in which the events are detected.
+    Returns
+    -------
+    q_values : iterable of scalar
+        The p-values adjusted using the FDR controlling procedure.
     """
-
-    def __init__(self, video, screen_detector, documents):
-        self._video = video
-        self._screen_detector = screen_detector
-        self._pages = list(chain(documents))
-
-    @property
-    def video(self):
-        return self._video
-
-    def __iter__(self):
-        # TODO
-        pass
+    p_value_array = np.asfarray(p_values)
+    num_pvalues = len(p_value_array)
+    descending_order = p_value_array.argsort()[::-1]
+    original_order = descending_order.argsort()
+    steps = num_pvalues / np.arange(num_pvalues, 0, -1)
+    descending_q_values = np.minimum.accumulate(steps * p_value_array[descending_order]).clip(0, 1)
+    q_values = descending_q_values[original_order]
+    return q_values
