d13d11e8717f78537593eccb9fbb3dc42eb1075a
Vít Novotný <witiko@mail.muni.cz>
2018-09-23 22:28:49 +0200

    Move benjamini_hochberg to video699.common

--- test/test_common.py
+++ test/test_common.py
@@ -3,7 +3,7 @@
 import unittest
 
 from numpy.testing import assert_array_almost_equal
-from video699.event.spearman import benjamini_hochberg
+from video699.common import benjamini_hochberg
 
 
 class TestBenjaminiHochberg(unittest.TestCase):
@@ -19,7 +19,7 @@ class TestBenjaminiHochberg(unittest.TestCase):
     def test_nonempty(self):
         p_values = (0.005, 0.009, 0.05, 0.1, 0.2, 0.3)
         q_values = tuple(benjamini_hochberg(p_values))
-        assert_array_almost_equal((0.027, 0.027, 0.100, 0.150, 0.240, 0.300), q_values)
+        assert_array_almost_equal((0.027, 0.027, 0.1, 0.15, 0.24, 0.3), q_values)
 
 
 if __name__ == '__main__':
--- video699/common.py
+++ video699/common.py
@@ -6,6 +6,8 @@
 
 from math import floor, ceil
 
+import numpy as np
+
 
 COLOR_RGBA_TRANSPARENT = (0, 0, 0, 0)
 
@@ -82,3 +84,40 @@ def timedelta_as_xsd_duration(timedelta):
         microseconds=timedelta.microseconds,
     )
     return duration
+
+
+def benjamini_hochberg(p_values):
+    """Adjusts p-values from independent hypothesis tests to q-values.
+
+    The q-values are determined using the false discovery rate (FDR) controlling procedure of
+    Benjamini and Hochberg [BenjaminiHochberg1995]_.
+
+    .. [BenjaminiHochberg1995] Benjamini, Yoav; Hochberg, Yosef (1995). "Controlling the false
+       discovery rate: a practical and powerful approach to multiple testing". Journal of the
+       Royal Statistical Society, Series B. 57 (1): 289–300. MR 1325392.
+
+    Notes
+    -----
+    This method was adapted from `code posted to Stack Overflow by Eric Talevich`_.
+
+    .. _code posted to Stack Overflow by Eric Talevich: https://stackoverflow.com/a/33532498/657401
+
+    Parameters
+    ----------
+    p_values : iterable of scalar
+        p-values from independent hypothesis tests.
+
+    Returns
+    -------
+    q_values : iterable of scalar
+        The p-values adjusted using the FDR controlling procedure.
+    """
+
+    p_value_array = np.asfarray(p_values)
+    num_pvalues = len(p_value_array)
+    descending_order = p_value_array.argsort()[::-1]
+    original_order = descending_order.argsort()
+    steps = num_pvalues / np.arange(num_pvalues, 0, -1)
+    descending_q_values = np.minimum.accumulate(steps * p_value_array[descending_order]).clip(0, 1)
+    q_values = descending_q_values[original_order]
+    return q_values
