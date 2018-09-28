73dd0e593253adcd8bb34b6d3667d3b2cb80a88a
Vít Novotný <witiko@mail.muni.cz>
2018-09-28 20:33:53 +0200

    Implement video699.event.pearson.RollingPearsonR

--- video699/event/pearson.py
+++ video699/event/pearson.py
@@ -9,10 +9,11 @@ rolling window of video frames. Related classes and methods are also implemented
 from collections import deque
 from itertools import chain
 from logging import getLogger
+from math import sqrt
 
 import cv2 as cv
 import numpy as np
-from scipy.stats import pearsonr
+from scipy.special import stdtr
 
 from ..common import benjamini_hochberg
 from ..configuration import get_configuration
@@ -26,6 +27,159 @@ CONFIGURATION = get_configuration()['RollingPearsonEventDetector']
 WINDOW_SIZE = int(CONFIGURATION['window_size'])
 SIGNIFICANCE_LEVEL = float(CONFIGURATION['significance_level'])
 SUBSAMPLE_SIZE = int(CONFIGURATION['subsample_size'])
+
+
+class RollingPearsonR(object):
+    """An efficient implementation of rolling weighted Pearson's correlation coefficient :math:`r`.
+
+    Parameters
+    ----------
+    window_size : int or None, optional
+        The size of the rolling window, i.e. the maximum number of previous time frames for which
+        :math:`r` is computed. If ``None`` or unspecified, then the number of time frames is
+        unbounded.
+    """
+
+    def __init__(self, window_size=None):
+        if window_size is not None and window_size < 1:
+            raise ValueError('The window size must not be less than one')
+        self._window_size = window_size
+
+        self._sample_x = deque(maxlen=window_size)
+        self._mean_x_sum = 0
+        self._mean_x2_sum = 0
+
+        self._sample_y = deque(maxlen=window_size)
+        self._mean_y_sum = 0
+        self._mean_y2_sum = 0
+
+        self._weights = deque(maxlen=window_size)
+        self._weights_sum = 0
+
+        self._mean_xy_sum = 0
+
+    def next(self, observations_x, observations_y, observation_weights, update=True):
+        r"""Adds provided observations to sample, moves to the next time frame, computes :math:`r`.
+
+        Parameters
+        ----------
+        observations_x : array_like
+            The provided observations of the first random variable.
+        observations_y : array_like
+            The provided observations of the second random variable.
+        observation_weights : array_like
+            Non-negative weights of the observations of the bivariate random vector.
+        update : bool, optional
+            If ``True``, then :math:`r` is computed, but the provided observations are not actually
+            added to the bivariate random sample, and we do not actually move to the next time
+            frame. If `False`` or unspecified, then the observation are added to the sample and we
+            move to the next time frame.
+
+        Returns
+        -------
+        correlation_coefficient : scalar
+            An estimate of the rolling weighted Pearson's correlation coefficient :math:`r`.
+        p_value : scalar
+            The probability of obtaining an estimate of :math:`r` that is at least as extreme as the
+            current estimate under the null hypothesis that :math:`r = 0`.
+
+        Raises
+        ------
+        ValueError
+            When ``observations_x``, ``observations_y``, and ``observation_weights`` have different
+            lengths.
+        """
+
+        if not len(observations_x) == len(observations_y) == len(observation_weights):
+            raise ValueError('Arrays containing bivariate observations must have the same length')
+
+        window_size = self._window_size
+
+        sample_x = self._sample_x
+        mean_x_sum = self._mean_x_sum
+        mean_x2_sum = self._mean_x2_sum
+
+        sample_y = self._sample_y
+        mean_y_sum = self._mean_y_sum
+        mean_y2_sum = self._mean_y2_sum
+
+        weights = self._weights
+        weights_sum = self._weights_sum
+
+        mean_xy_sum = self._mean_xy_sum
+
+        if len(sample_x) == window_size:
+            popped_observations_x = np.asfarray(sample_x[0])
+            popped_observations_y = np.asfarray(sample_y[0])
+            popped_weights = np.asfarray(weights[0])
+
+            mean_x_sum -= popped_observations_x.dot(popped_weights)
+            mean_x2_sum -= (popped_observations_x**2).dot(popped_weights)
+
+            mean_y_sum -= popped_observations_y.dot(popped_weights)
+            mean_y2_sum -= (popped_observations_y**2).dot(popped_weights)
+
+            weights_sum -= popped_weights.sum()
+
+            mean_xy_sum -= (popped_weights * popped_observations_x).dot(popped_observations_y)
+
+        observations_x = np.asfarray(observations_x)
+        observations_y = np.asfarray(observations_y)
+        observation_weights = np.asfarray(observation_weights)
+
+        mean_x_sum += observations_x.dot(observation_weights)
+        mean_x2_sum += (observations_x**2).dot(observation_weights)
+
+        mean_y_sum += observations_y.dot(observation_weights)
+        mean_y2_sum += (observations_y**2).dot(observation_weights)
+
+        weights_sum += observation_weights.sum()
+
+        mean_xy_sum += (observation_weights * observations_x).dot(observations_y)
+
+        degrees_of_freedom = weights_sum - 2
+
+        if degrees_of_freedom > 0:
+            mean_x = mean_x_sum / weights_sum
+            mean_x2 = mean_x2_sum / weights_sum
+            var_x = mean_x2 - mean_x**2
+            std_x = sqrt(var_x)
+
+            mean_y = mean_y_sum / weights_sum
+            mean_y2 = mean_y2_sum / weights_sum
+            var_y = mean_y2 - mean_y**2
+            std_y = sqrt(var_y)
+
+            mean_xy = mean_xy_sum / weights_sum
+            cov_xy = mean_xy - (mean_x * mean_y)
+
+            pearsonr = np.clip(cov_xy / (std_x * std_y), 0, 1)
+
+            if pearsonr < 1:
+                t_star = pearsonr * sqrt(degrees_of_freedom) / sqrt(1 - pearsonr**2)
+                p_left_tail = stdtr(degrees_of_freedom, -abs(t_star))
+                p_value = np.clip(2 * p_left_tail, 0, 1)
+            else:
+                p_value = 0.0
+        else:
+            pearsonr = 0.0
+            p_value = 0.0
+
+        if update:
+            sample_x.append(observations_x)
+            self._mean_x_sum = mean_x_sum
+            self._mean_x2_sum = mean_x2_sum
+
+            sample_y.append(observations_y)
+            self._mean_y_sum = mean_y_sum
+            self._mean_y2_sum = mean_y2_sum
+
+            weights.append(observation_weights)
+            self._weights_sum = weights_sum
+
+            self._mean_xy_sum = mean_xy_sum
+
+        return pearsonr, p_value
 
 
 class RollingPearsonPageDetector(PageDetectorABC):
