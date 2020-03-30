0256712fc154a88f4eedfecdbd6ce41a3dfc2658
mikulas <mikulas.bankovic@simplefinance.io>
2020-03-30 11:37:24 +0200

    params changed

--- test/screen/test_fastai_detector.py
+++ test/screen/test_fastai_detector.py
@@ -6,6 +6,7 @@ from video699.interface import ScreenDetectorABC
 from video699.screen.semantic_segmentation.fastai_detector import FastAIScreenDetector, ALL_VIDEOS
 import numpy as np
 
+
 class TestFastAIScreenDetector(unittest.TestCase):
     """
     Tests the ability of the AnnotatedScreenVideo class to detect its dimensions and produce frames.
@@ -26,7 +27,7 @@ class TestFastAIScreenDetector(unittest.TestCase):
         self.assertTrue(self.detector.labels_path.exists())
         self.assertTrue(self.detector.videos_path.exists())
 
-    def test_default_params(self):
+    def test_init_params(self):
         self.assertIsNotNone(self.detector.methods)
         self.assertIsInstance(self.detector.methods, dict)
         all_params = {'base', 'base_lower_bound', 'base_upper_bound', 'base_factors', 'erode_dilate',
@@ -38,6 +39,19 @@ class TestFastAIScreenDetector(unittest.TestCase):
         all_train_params = {'batch_size', 'resize_factor', 'frozen_epochs', 'unfrozen_epochs',
                             'frozen_lr', 'unfrozen_lr'}
         self.assertSetEqual(set(self.detector.train_params.keys()), all_train_params)
+
+    def test_init_params_custom(self):
+        self.detector = FastAIScreenDetector(train_params={'batch_size': 4})
+        all_params = {'base', 'base_lower_bound', 'base_upper_bound', 'base_factors', 'erode_dilate',
+                      'erode_dilate_lower_bound', 'erode_dilate_upper_bound', 'erode_dilate_factors',
+                      'erode_dilate_iterations', 'ratio_split', 'ratio_split_lower_bound', 'ratio_split_upper_bound'}
+
+        self.assertSetEqual(set(self.detector.methods.keys()), all_params)
+
+        all_train_params = {'batch_size', 'resize_factor', 'frozen_epochs', 'unfrozen_epochs',
+                            'frozen_lr', 'unfrozen_lr'}
+        self.assertSetEqual(set(self.detector.train_params.keys()), all_train_params)
+        self.assertEqual(self.detector.train_params['batch_size'], 4)
 
     def test_save_load(self):
         self.detector.train()
