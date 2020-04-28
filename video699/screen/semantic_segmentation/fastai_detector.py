25d32e1c8074b077c2e213b32952ff025364f672
mikulas <mikulas.bankovic@simplefinance.io>
2020-04-28 18:41:06 +0200

    default transformation changed

--- video699/screen/semantic_segmentation/fastai_detector.py
+++ video699/screen/semantic_segmentation/fastai_detector.py
@@ -231,8 +231,9 @@ class FastAIScreenDetector(ScreenDetectorABC):
     def init_model(self):
         size = self.src_shape // self.train_params['resize_factor']
         tfms = get_transforms(do_flip=True, flip_vert=False, max_rotate=10.0,
-                              max_zoom=1.0, max_lighting=0.4, max_warp=1,
-                              p_affine=0, p_lighting=0.75)
+                              max_zoom=1.0, max_lighting=0.8, max_warp=1,
+                              p_affine=0, p_lighting=0.5)
+        tfms = (tfms[0][1:], tfms[1])
 
         get_label = partial(get_label_from_image_name, self.labels_path)
 
